0fd7f5956fe808055057c46d5ca4ad3b74dcab60
NATASHA ZYWCZYK <G00415696@atu.ie>
2025-11-21 15:06:45 +0000

    For typscript to acknowledge .vue files

---
+++ CraveControl/src/shims-vue.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+/// <reference types="vite/client" />
+/// <reference types="vue/ref-macros" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
