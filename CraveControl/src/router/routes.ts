273530851a20033efa17fdea48cc3464e1b32f71
NATASHA ZYWCZYK <G00415696@atu.ie>
2025-11-28 16:18:50 +0000

    Added routing to signup page

--- CraveControl/src/router/routes.ts
+++ CraveControl/src/router/routes.ts
@@ -7,6 +7,7 @@ const routes: RouteRecordRaw[] = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: 'login', component: () => import('pages/LoginPage.vue') },
+      { path: 'signup', component: () => import('pages/SignupPage.vue') },
     ],
   },
 
