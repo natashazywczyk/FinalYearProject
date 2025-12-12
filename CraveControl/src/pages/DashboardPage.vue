9c7df1b72025336502905c02678f7ec4b695058f
NATASHA ZYWCZYK <G00415696@atu.ie>
2025-12-12 22:07:32 +0000

    Basic page for dashboard

---
+++ CraveControl/src/pages/DashboardPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="q-pa-md">
+    <div class="row justify-center">
+      <div class="col-12 col-md-10">
+        <div class="text-h4 q-mb-md">Welcome to CraveControl</div>
+        <div class="text-subtitle1 q-mb-lg text-grey-7">
+          Track your cravings and view your progress!
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+//mport { useQuasar } from 'quasar';
+
+//const $q = useQuasar();
+</script>
