ccf7d37f888d38a02907e0a9bde1cc04d1276be9
NATASHA ZYWCZYK <G00415696@atu.ie>
2026-01-09 21:10:46 +0000

    Visual representation of 'Savings Pot' for accumulative savings with amount

--- CraveControl/src/pages/SavingsPage.vue
+++ CraveControl/src/pages/SavingsPage.vue
@@ -10,6 +10,21 @@
           Track how much money you've saved by resisting your cravings
         </div>
 
+        <div class="row justify-center q-mt-xl q-mb-xl">
+          <div class="col-12 col-md-6 text-center">
+            <div style="position: relative; display: inline-block; max-width: 500px; width: 100%;">
+              <img
+                src="/images/savings-pot.png"
+                alt="Pot of Gold"
+                style="width: 100%; display: block;"
+              />
+              <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                <div class="text-h3 text-white" style="font-weight: bold; text-shadow: 2px 2px 4px rgba(0,0,0,0.5);">€0.00</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   </q-page>
