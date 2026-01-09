bbf95a0146533051d02d8687db8bcb69d92bd72b
NATASHA ZYWCZYK <G00415696@atu.ie>
2026-01-09 17:34:05 +0000

    Added cards for navigation to core functionalities

--- CraveControl/src/pages/DashboardPage.vue
+++ CraveControl/src/pages/DashboardPage.vue
@@ -6,6 +6,50 @@
         <div class="text-subtitle1 q-mb-lg text-grey-7">
           Track your cravings and view your progress!
         </div>
+
+        <div class="row q-col-gutter-md">
+          <div class="col-12 col-md-4">
+            <q-card class="cursor-pointer" flat bordered>
+              <q-card-section class="text-center">
+                <div class="text-h6">Check your savings</div>
+                <div class="text-caption text-grey-7 q-mt-sm">
+                  See how much money you've saved by controlling your cravings
+                </div>
+              </q-card-section>
+              <q-card-actions align="center">
+                <q-btn color="primary" label="View Savings" unelevated />
+              </q-card-actions>
+            </q-card>
+          </div>
+
+          <div class="col-12 col-md-4">
+            <q-card class="cursor-pointer" flat bordered>
+              <q-card-section class="text-center">
+                <div class="text-h6">Craving Tracking</div>
+                <div class="text-caption text-grey-7 q-mt-sm">
+                  Enter and track your daily craving level
+                </div>
+              </q-card-section>
+              <q-card-actions align="center">
+                <q-btn color="primary" label="Log Craving Level" unelevated />
+              </q-card-actions>
+            </q-card>
+          </div>
+
+          <div class="col-12 col-md-4">
+            <q-card class="cursor-pointer" flat bordered>
+              <q-card-section class="text-center">
+                <div class="text-h6">Learn more</div>
+                <div class="text-caption text-grey-7 q-mt-sm">
+                  TBA
+                </div>
+              </q-card-section>
+              <q-card-actions align="center">
+                <q-btn color="primary" label="Learn More" unelevated />
+              </q-card-actions>
+            </q-card>
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
