eca8abbb24b03be49877275473e8fe86f26688ed
NATASHA ZYWCZYK <G00415696@atu.ie>
2026-01-14 15:42:10 +0000

    Removed bloat

--- CraveControl/src/router/index.ts
+++ CraveControl/src/router/index.ts
@@ -8,16 +8,8 @@ import {
 import routes from './routes';
 import { supabase } from 'src/boot/supabase';
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
 
-export default defineRouter(function (/* { store, ssrContext } */) {
+export default defineRouter(function () {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
     : process.env.VUE_ROUTER_MODE === 'history'
@@ -28,9 +20,6 @@ export default defineRouter(function (/* { store, ssrContext } */) {
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
 
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE),
   });
 
