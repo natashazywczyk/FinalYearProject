d0403fb5bb59fee81e4f93a554708fe988cf814b
NATASHA ZYWCZYK <G00415696@atu.ie>
2026-02-04 21:40:56 +0000

    Routing to new pages

--- CraveControl/src/router/routes.ts
+++ CraveControl/src/router/routes.ts
@@ -8,11 +8,16 @@ const routes: RouteRecordRaw[] = [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: 'login', component: () => import('pages/LoginPage.vue') },
       { path: 'signup', component: () => import('pages/SignupPage.vue') },
+      { path: 'auth-callback', component: () => import('pages/AuthCallbackPage.vue') },
+      { path: 'complete-profile', component: () => import('pages/CompleteProfilePage.vue') },
       { path: 'dashboard', component: () => import('pages/DashboardPage.vue') },
       { path: 'savings', component: () => import('pages/SavingsPage.vue') },
       { path: 'log-craving', component: () => import('pages/CravingTrackingPage.vue') },
       { path: 'information', component: () => import('pages/InformationPage.vue') },
-      { path: 'product-selection', component: () => import('pages/NicotineProductSelectionPage.vue') },
+      {
+        path: 'product-selection',
+        component: () => import('pages/NicotineProductSelectionPage.vue'),
+      },
     ],
   },
 
